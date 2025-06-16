b2348fea3fdc3996748469bfb725603a2cfd9109
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2025-06-16 12:52:48 -0400

    update: added new default SwiftQueuePool

--- src/SwiftCollections/Pool/DefaultImplementations/SwiftQueuePool.cs
+++ src/SwiftCollections/Pool/DefaultImplementations/SwiftQueuePool.cs
@@ -4,25 +4,25 @@ using System.Threading;
 namespace SwiftCollections.Pool
 {
     /// <summary>
-    /// A specialized pool for managing <see cref="SwiftHashSet{T}"/> instances, 
+    /// A specialized pool for managing <see cref="SwiftQueue{T}"/> instances, 
     /// providing efficient reuse to minimize memory allocations and improve performance.
     /// </summary>
-    /// <typeparam name="T">The type of elements in the hash set.</typeparam>
-    public sealed class SwiftHashSetPool<T> : SwiftCollectionPool<SwiftHashSet<T>, T>, IDisposable
+    /// <typeparam name="T">The type of elements in the queue.</typeparam>
+    public sealed class SwiftQueuePool<T> : SwiftCollectionPool<SwiftQueue<T>, T>, IDisposable
     {
         #region Singleton Instance
 
         /// <summary>
-        /// Shared instance of the hash set pool, providing a globally accessible pool.
+        /// Shared instance of the queue pool, providing a globally accessible pool.
         /// Uses <see cref="LazyDisposable{T}"/> to ensure lazy initialization and proper disposal.
         /// </summary>
-        private readonly static LazyDisposable<SwiftHashSetPool<T>> _lazyInstance =
-            new(() => new SwiftHashSetPool<T>(), LazyThreadSafetyMode.ExecutionAndPublication);
+        private readonly static LazyDisposable<SwiftQueuePool<T>> _lazyInstance =
+            new(() => new SwiftQueuePool<T>(), LazyThreadSafetyMode.ExecutionAndPublication);
 
         /// <summary>
         /// Gets the shared instance of the pool.
         /// </summary>
-        public static SwiftHashSetPool<T> Shared => _lazyInstance.Value;
+        public static SwiftQueuePool<T> Shared => _lazyInstance.Value;
 
         #endregion
 
@@ -38,37 +38,37 @@ namespace SwiftCollections.Pool
         #region Methods
 
         /// <summary>
-        /// Rents a hash set instance from the pool. If the pool is empty, a new hash set is created.
+        /// Rents a queue instance from the pool. If the pool is empty, a new queue is created.
         /// </summary>
-        /// <returns>A <see cref="SwiftHashSet{T}"/> instance.</returns>
-        public override SwiftHashSet<T> Rent()
+        /// <returns>A <see cref="SwiftQueue{T}"/> instance.</returns>
+        public override SwiftQueue<T> Rent()
         {
             if (_disposed)
-                ThrowHelper.ThrowObjectDisposedException(nameof(SwiftHashSetPool<T>));
+                ThrowHelper.ThrowObjectDisposedException(nameof(SwiftQueuePool<T>));
 
             return base.Rent();
         }
 
         /// <summary>
-        /// Releases a hash set instance back to the pool for reuse.
+        /// Releases a queue instance back to the pool for reuse.
         /// </summary>
-        /// <param name="set">The hash set to release.</param>
+        /// <param name="queue">The queue to release.</param>
         /// <remarks>
-        /// The hash set will be cleared before being returned to the pool to ensure it contains no stale data.
+        /// The queue will be cleared before being returned to the pool to ensure it contains no stale data.
         /// </remarks>
-        public override void Release(SwiftHashSet<T> set)
+        public override void Release(SwiftQueue<T> queue)
         {
             if (_disposed)
-                ThrowHelper.ThrowObjectDisposedException(nameof(SwiftHashSetPool<T>));
+                ThrowHelper.ThrowObjectDisposedException(nameof(SwiftQueuePool<T>));
 
-            if (set == null) return;
+            if (queue == null) return;
 
             // SwiftCollectionPool clears pool before releasing
-            base.Release(set);
+            base.Release(queue);
         }
 
         /// <summary>
-        /// Clears all pooled hash sets from the pool.
+        /// Clears all pooled queues from the pool.
         /// </summary>
         public override void Clear()
         {
@@ -82,7 +82,7 @@ namespace SwiftCollections.Pool
         #region IDisposable Implementation
 
         /// <summary>
-        /// Releases all resources used by the SwiftHashSetPool.
+        /// Releases all resources used by the SwiftQueuePool.
         /// It is important to call Dispose() to release pooled objects, preventing potential memory leaks.
         /// </summary>
         public void Dispose()
@@ -99,7 +99,7 @@ namespace SwiftCollections.Pool
             GC.SuppressFinalize(this);
         }
 
-        ~SwiftHashSetPool() => Dispose();
+        ~SwiftQueuePool() => Dispose();
 
         #endregion
     }
