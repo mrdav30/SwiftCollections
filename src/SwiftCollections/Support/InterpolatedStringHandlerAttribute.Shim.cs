5481fdd03aae1077b4c10db70a2d985b807148a1
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2026-05-07 11:17:10 -0400

    task: added shim for netstandard

--- src/SwiftCollections/Support/InterpolatedStringHandlerAttribute.Shim.cs
+++ src/SwiftCollections/Support/InterpolatedStringHandlerAttribute.Shim.cs
@@ -3,24 +3,41 @@ namespace System.Runtime.CompilerServices;
 
 using System;
 
+/// <summary>
+/// Provides the compiler-recognized interpolated string handler marker for target frameworks that do not define it publicly.
+/// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
-internal sealed class InterpolatedStringHandlerAttribute : Attribute
+public sealed class InterpolatedStringHandlerAttribute : Attribute
 {
 }
 
+/// <summary>
+/// Provides the compiler-recognized argument binding marker for target frameworks that do not define it publicly.
+/// </summary>
 [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
-internal sealed class InterpolatedStringHandlerArgumentAttribute : Attribute
+public sealed class InterpolatedStringHandlerArgumentAttribute : Attribute
 {
+    /// <summary>
+    /// Initializes the attribute with one constructor argument name.
+    /// </summary>
+    /// <param name="argument">The method parameter name to pass to the interpolated string handler constructor.</param>
     public InterpolatedStringHandlerArgumentAttribute(string argument)
     {
         Arguments = new[] { argument };
     }
 
+    /// <summary>
+    /// Initializes the attribute with multiple constructor argument names.
+    /// </summary>
+    /// <param name="arguments">The method parameter names to pass to the interpolated string handler constructor.</param>
     public InterpolatedStringHandlerArgumentAttribute(params string[] arguments)
     {
         Arguments = arguments;
     }
 
+    /// <summary>
+    /// Gets the method parameter names passed to the interpolated string handler constructor.
+    /// </summary>
     public string[] Arguments { get; }
 }
 #endif
